b518ecefd1a9113699600058f9906f63a5fcf278
bloombar <amos@cs.nyu.edu>
2023-02-10 22:41:16 -0500

    better instructions

--- src/edu/nyu/cs/CompoundInflation.java
+++ src/edu/nyu/cs/CompoundInflation.java
@@ -16,9 +16,11 @@ import java.util.Scanner;
  *  - (i.e. the value of each dollar decreases every month at a monthly rate of 0.10/12 = 0.008333333333333)
  *  - use (int) Math.round(someNumberWithDecimalPlaces) to round all displayed numbers to the nearest dollar... do not output any decimal places.
  *  - assume the user will enter a valid number with or without a decimal place... don't worry about handling any other type of user input
+ *  - use System.out.println() to output all text, not System.out.print()
  * 
  * Sample output, including example user input. Your program should format the output exactly like this: 
- *  Please enter your monthly savings rate: 120
+ *  Please enter your monthly savings rate: 
+ *  120
  *  If you save $120 per month with 10% annual inflation, after 6 months, your account will hold an amount equivalent to $114 today.
  */
 public class CompoundInflation {
